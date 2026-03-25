fcba2a576b636d3d652160d8952e70152610028b
ploor05 <pauloorbermeo@gmail.com>
2026-03-25 19:59:23 +0100

    Dades mock Afegides

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,14 @@
+import { Element } from '../models/element.model';
+
+export const DADES_MOCK: Element[] = [
+  { id: 1, name: 'Element 1', value: 10, description: 'Primer element' },
+  { id: 2, name: 'Element 2', value: 20 },
+  { id: 3, name: 'Element 3', value: 15, description: 'Amb descripció' },
+  { id: 4, name: 'Element 4', value: 8 },
+  { id: 5, name: 'Element 5', value: 12 },
+  { id: 6, name: 'Element 6', value: 30, description: 'Important' },
+  { id: 7, name: 'Element 7', value: 5 },
+  { id: 8, name: 'Element 8', value: 18 },
+  { id: 9, name: 'Element 9', value: 22, description: 'Element destacat' },
+  { id: 10, name: 'Element 10', value: 14 }
+];
